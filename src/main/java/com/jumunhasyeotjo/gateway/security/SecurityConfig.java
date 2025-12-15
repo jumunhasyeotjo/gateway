54b25a4edad4f9c79b90e21fac0173416b22e507
‘seokjun7410’ <47974623+seokjun7410@users.noreply.github.com>
2025-12-15 16:10:20 +0900

    feat: security 유레카 authorizeExchange 설정

--- src/main/java/com/jumunhasyeotjo/gateway/security/SecurityConfig.java
+++ src/main/java/com/jumunhasyeotjo/gateway/security/SecurityConfig.java
@@ -27,7 +27,7 @@ public class SecurityConfig {
             .httpBasic(ServerHttpSecurity.HttpBasicSpec::disable)
             .securityContextRepository(NoOpServerSecurityContextRepository.getInstance())
             .authorizeExchange(ex -> ex
-                .pathMatchers("/api/v1/auth/**", "/actuator/**", "/monitoring/**").permitAll()
+                .pathMatchers("/api/v1/auth/**", "/actuator/**", "/monitoring/**", "/eureka/").permitAll()
                 .anyExchange().authenticated()
             )
             .addFilterAt(jwtAuthenticationFilter, SecurityWebFiltersOrder.AUTHENTICATION)
