a0a242c1ffda9c826f7064f714f17c5f6f6f3eed
Namyejun <yejun1148@gmail.com>
2025-12-16 19:31:03 +0900

    feat : 내부 ALB 타도록 변경

--- src/main/java/com/jumunhasyeotjo/gateway/GatewayConfig.java
+++ src/main/java/com/jumunhasyeotjo/gateway/GatewayConfig.java
@@ -22,7 +22,7 @@ public class GatewayConfig {
         return builder.routes()
                 .route("user-service", r -> r
                         .path("/api/v1/auth/**", "/api/v1/users/**", "/api/v1/messages/**", "/api/v1/passports/**")
-                        .uri("http://" + albUrl + ":" + userPort)
+                        .uri(albUrl + ":" + userPort)
                 )
                 .route("user-service-actuator", r -> r
                         .path("/monitoring/user-service/actuator/**")
@@ -30,15 +30,15 @@ public class GatewayConfig {
                                 "/monitoring/user-service/(?<segment>.*)",
                                 "/${segment}"
                         ))
-                        .uri("http://" + albUrl + ":" + userPort)
+                        .uri(albUrl + ":" + userPort)
                 )
                 .route("order-to-shipping-service", r -> r
                         .path("/api/v1/orders/**", "/api/v1/coupons/**", "/api/v1/shippings/**", "/api/v1/payments/**", "/api/v1/shipping-histories/**")
-                        .uri("http://" + albUrl + ":" + orderPort)
+                        .uri(albUrl + ":" + orderPort)
                 )
                 .route("hub-product-stock-company", r -> r
                         .path("/api/v1/hubs/**", "/api/v1/stocks/**", "/api/v1/products/**", "/api/v1/companies/**")
-                        .uri("http://" + albUrl + ":" + hubPort)
+                        .uri(albUrl + ":" + hubPort)
                 )
                 .build();
     }
